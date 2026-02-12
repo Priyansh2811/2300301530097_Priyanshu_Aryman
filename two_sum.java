338901e2516b3c0969b7251fff8406836b37ca62
Your Name <your.email@example.com>
2026-02-12 12:13:52 +0530

    Two sum

---
+++ two_sum.java
@@ -0,0 +1,43 @@
+
+import java.util.HashMap;
+import java.util.Map;
+import java.util.Scanner;
+
+public class two_sum{
+    public static void main(String[] args) {
+        Scanner scan = new Scanner(System.in);
+        System.out.print("Enter the size of the array: ");
+        int size = scan.nextInt();
+        int[] nums = new int[size];
+        System.out.println("Enter the elements of the array: ");
+        for (int i = 0; i < size; i++) {
+            nums[i] = scan.nextInt();
+        }
+        System.out.print("Enter the target sum: ");
+        int target = scan.nextInt();
+        
+        int[] result = twoSum(nums, target);
+        
+        if (result != null) {
+            System.out.println("Indices of the two numbers that add up to " + target + ": " + result[0] + ", " + result[1]);
+        } else {
+            System.out.println("No two numbers add up to " + target);
+        }
+    }
+    
+    public static int[] twoSum(int[] nums, int target) {
+        Map<Integer, Integer> map = new HashMap<>();
+        
+        for (int i = 0; i < nums.length; i++) {
+            int complement = target - nums[i];
+            
+            if (map.containsKey(complement)) {
+                return new int[] { map.get(complement), i };
+            }
+            
+            map.put(nums[i], i);
+        }
+        
+        return null; // No solution found
+    }
+}
